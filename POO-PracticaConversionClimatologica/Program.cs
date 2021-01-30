6ce6a0b373064711fc21d8cfa7816884e9064552
Carlos Alexander Gonzalez Garcia <carlosalexandergonzalezzgarcia@gmail.com>
2021-01-30 17:49:51 -0600

    Adición-Comentario de Copyright

--- POO-PracticaConversionClimatologica/Program.cs
+++ POO-PracticaConversionClimatologica/Program.cs
@@ -1,4 +1,18 @@
-﻿using System;
+﻿/*
+ * Copyright (C) 2021-2022 Carlos Alexander carlosalexandergonzalezzgarcia@gmail.com 
+ * Este programa es software gratuito; puedes redistribuirlo y/o modificar bajo los términos de la Licencia Pública 
+ * General GNU publicada por Free Software Foundation; ya sea la versión 3 de la licencia, o (a su elección) cualquier 
+ * versión posterior. 
+ * 
+ * Este programa se distribuye con la esperanza de que sea útil, pero SIN NINGUNA GARANTÍA; sin siquiera la garantía 
+ * implícita de COMERCIABILIDAD o APTITUD PARA UN PROPÓSITO PARTICULAR. Ver la Licencia pública general GNU 
+ * para más detalles. 
+ * 
+ * Debería haber recibido una copia de la Licencia Pública General GNU junto con este programa; en caso contrario, 
+ * escriba a Free Software Foundation, Inc., 51 Franklin Street, quinto piso, Boston, MA 02110-1301 EE.UU.
+*/
+
+using System;
 
 namespace Conversion {
 
