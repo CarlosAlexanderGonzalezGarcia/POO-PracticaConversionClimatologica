2d7469d7e63bf4fe8311c962a68753d5c0330710
Carlos Alexander Gonzalez Garcia <carlosalexandergonzalezzgarcia@gmail.com>
2021-01-30 17:42:17 -0600

    Correccion ortografica en celsius, correcciones en las formulas eh impresion de los datos, fusion de los metodos OperacionFahrenheit y OperacionKelvin en un solo metodo Operacion

--- POO-PracticaConversionClimatologica/Program.cs
+++ POO-PracticaConversionClimatologica/Program.cs
@@ -1,59 +1,52 @@
 ﻿using System;
 
-namespace Conversion
-{
+namespace Conversion {
+
     //Cambio el nombre de class Program que se crea por defecto a class Temp
-    class Temp
-    {
+    class Temp {
+
         //Creo los atributos que nesecitare en esta clase
-        private int celsius;
-        private float fahrenheit, kelvin;
+        private float fahrenheit, kelvin, celsius;
 
         //Creo mi primer metodo, el cual capturara la temperatura en Celsius
-        public void Captura()
-        {
+        public void Captura() {
+
             //Pido por teclado el dato que requiero
-            Console.Write("Ingresa la temperatura en Celcius: ");
+            Console.Write("Ingresa la temperatura en Celsius: ");
             //Lo guardo en el atributo celsius
-            celsius = int.Parse(Console.ReadLine());
+            celsius = float.Parse(Console.ReadLine());
         }
-        //Creo mi segundo metodo el cual realizara la conversion de celsius a fahrenheit eh imprimira el resultado
-        public void OperacionFarenheint()
-        {
-            //Inicio mi atributo fahrenheit dentro de este metodo
+
+        //Creo mi segundo metodo el cual realizara la conversion de celsius a fahrenheit y kelvin, por ultimo imprimira el resultado
+        public void Operacion() {
+
+            //Inicio mi atributo fahrenheit dentro de mi metodo Operacion
             fahrenheit = 32.00F;
-            //Realizo la conversion
-            fahrenheit *= celsius;
+            //Inicio mi atributo kelvin dentro de mi metodo Operacion
+            kelvin = 273.15F;
+            //Realizo la conversiona a fahrenheit
+            fahrenheit += (celsius * 9 / 5);
+            //Realizo la conversion a kelvin
+            kelvin += celsius;
             //Imprimo el resultado
-            Console.Write("\n" + celsius + "° Celsius es igual a: " + fahrenheit + "°" + " Fahrenheit" + "\n" + "\n");
+            Console.Write("\n" + celsius + "° Celsius es igual a: " + fahrenheit + "°" + "F" + "\n" + "\n" +
+                                 celsius + "° Celsius es igual a: " + kelvin + "°" + "K" + "\n" + "\n");
         }
-        //Creo mi tercer metodo el cual realizara la conversion de celsius a kelvin eh imprimira el resultado
-        public void OperacionKelvin()
-        {
-            //Inicio mi atributo kelvin dentro de este metodo
-            kelvin = 274.15F;
-            //Realizo la conversion
-            kelvin *= celsius;
-            //Imprimo el resultado
-            Console.Write(celsius + "° Celsius es igual a: " + kelvin + "°" + " Kelvin" + "\n" + "\n");
-        }
+
         //Por ultimo en mi funcion principal llamo a mis metodos creados
-        static void Main(string[] args)
-        {
+        static void Main(string[] args) {
+
             //Creo mi instancia la cual llamara a mis metodos
             Temp fin = new Temp();
             //Hago que fin llame al metodo captura
             fin.Captura();
             //Hago que fin llame al metodo OperacionFarenheint
-            fin.OperacionFarenheint();
-            //Hago que fin llame al metodo OperacionKelvin
-            fin.OperacionKelvin();
-
-            //fin
+            fin.Operacion();
 
             //Salgo del programa
             Console.Write("Oprime cualquier tecla para salir.");
             Console.ReadKey();
         }
     }
+    //fin de mi clase Temp
 }
